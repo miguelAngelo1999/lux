c6047416204d1c01b8556f7ecf06976895b5dc13
miguelAngelo1999 <miguelAngelo1999@users.noreply.github.com>
2026-06-22 18:18:30 -0300

    fix: add universal binary checksum for macOS (fixes checksum mismatch on fresh installs)

--- lib/core/checksum.dart
+++ lib/core/checksum.dart
@@ -5,6 +5,7 @@ import 'package:crypto/crypto.dart';
 // checksum-start
  const darwinAmd64Checksum = "6146982cd80750dff36ccba6b3e1aa117a2d2e2c539c758187acf226442a9a49";
  const darwinArm64Checksum = "c4d39d5f3dacc662773385e8dc4cc1a2206e5d5b44f26275d4a713d1639e140f";
+ const darwinUniversalChecksum = "e0507462561ee33490a411311f6384bc8bd6c45254ca4f5cfa87b8b9cbb3f6ec";
  const windowsAmd64Checksum = "6428607b8ff768edd85c5fd03c05925ef48dd10cbd58e24dfabae72105c1348e";
 // checksum-end
 
@@ -21,6 +22,7 @@ Future<void> verifyCoreBinary(String filePath) async {
   } else {
     validChecksums.add(darwinAmd64Checksum);
     validChecksums.add(darwinArm64Checksum);
+    validChecksums.add(darwinUniversalChecksum);
   }
   if (!validChecksums.contains(curChecksum)) {
     throw "Checksum of core binary is not matched. Expect $validChecksums, get $curChecksum.";
