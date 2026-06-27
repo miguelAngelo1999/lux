2995f2f9c0d852550def61a989961577140dd9dd
miguelAngelo1999 <miguelAngelo1999@users.noreply.github.com>
2026-06-27 16:59:13 -0300

    fix(windows): update checksum 371e388 — adds bypass_all rule file

--- lib/core/checksum.dart
+++ lib/core/checksum.dart
@@ -6,7 +6,7 @@ import 'package:crypto/crypto.dart';
  const darwinAmd64Checksum = "6146982cd80750dff36ccba6b3e1aa117a2d2e2c539c758187acf226442a9a49";
  const darwinArm64Checksum = "c4d39d5f3dacc662773385e8dc4cc1a2206e5d5b44f26275d4a713d1639e140f";
  const darwinUniversalChecksum = "88a653cccca1ef67ded1f5475ed1236eefff718fffdda39ef5e8ad1b5e09ea94";
- const windowsAmd64Checksum = "f646a20b8707870fb5b811d87b7ba6630d7490b796ee76388e894b9d2deb0b6b";
+ const windowsAmd64Checksum = "371e388a9a9b1f2432c725085fe2f117e98041de4deb631953e23f2ccf921d8c";
 // checksum-end
 
 Future<void> verifyCoreBinary(String filePath) async {
