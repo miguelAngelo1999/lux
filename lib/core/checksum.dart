120efd71ae8f387d04f183bd554b04fc618ac544
miguelAngelo1999 <miguelAngelo1999@users.noreply.github.com>
2026-07-05 13:53:33 -0300

    fix: rebuild lux_core with CONNECT retry + 407 logging

--- lib/core/checksum.dart
+++ lib/core/checksum.dart
@@ -5,7 +5,7 @@ import 'package:crypto/crypto.dart';
 // checksum-start
  const darwinAmd64Checksum = "6146982cd80750dff36ccba6b3e1aa117a2d2e2c539c758187acf226442a9a49";
  const darwinArm64Checksum = "c4d39d5f3dacc662773385e8dc4cc1a2206e5d5b44f26275d4a713d1639e140f";
- const darwinUniversalChecksum = "040ce2a88207b6525b3446d426efe0f586e912a8188ed837bb0945dd10bfaed4";
+ const darwinUniversalChecksum = "bc8b7d8ada673985ef41713deba4ee310493651ae66863b12449fcd8c781424f";
  const windowsAmd64Checksum = "8e11a43ebdb0bf35b4e400e6e62c75d72d14e2bb7ae61da2fdd8e000c78bf673";
 // checksum-end
 
