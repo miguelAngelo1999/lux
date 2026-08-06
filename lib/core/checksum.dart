36658cb5d5d316eaf4ffff5c5b8a3fce278b0e4d
miguelAngelo1999 <miguelAngelo1999@users.noreply.github.com>
2026-08-06 13:44:33 -0300

    chore: update Windows checksum to match current lux_core binary

--- lib/core/checksum.dart
+++ lib/core/checksum.dart
@@ -6,7 +6,7 @@ import 'package:crypto/crypto.dart';
  const darwinAmd64Checksum = "d20faade89b5974a0f07d55de8e12f7e452ec2fe14a2c9d241000823cbc5667f";
  const darwinArm64Checksum = "36547c73404c3a314c1e22e85e62144096e49aafb39f380536d8b8955f8d3c9b";
  const darwinUniversalChecksum = "653fd260348c0c9139a91c36a6309fb09c6a4c889e13664ae5c740f457ae2794";
- const windowsAmd64Checksum = "aabf7e9b938e7bf5bb41b6f1eaa36118ab012de9d2bff98acf6c44bb9f4a4c1b";
+ const windowsAmd64Checksum = "86c05e0aa55c2121cc7a85152714b55566237bef694f81f77f224ff188732f62";
 // checksum-end
 
 Future<void> verifyCoreBinary(String path) async {
