a68cffc507d93037f632e907c10c37fa10230e84
miguelAngelo1999 <miguelAngelo1999@users.noreply.github.com>
2026-08-11 16:59:31 -0300

    chore: resync windows core checksum after per-proxy PAC

--- lib/core/checksum.dart
+++ lib/core/checksum.dart
@@ -5,7 +5,7 @@ import 'package:crypto/crypto.dart';
 // checksum-start
  const darwinAmd64Checksum = "270dcd06f8de7767c0d4cc7a0ab0f61ba47b54f884340442f1bbc052ef35f3d2";
  const darwinArm64Checksum = "1d2e3ebdc05c884df2effc92909300241a9b917f25d7da398f8ebed1fea544ab";
- const windowsAmd64Checksum = "d5bb61cd7e414164aca01f635db3feedcf6e4cdb01f0043e37ca9cc8e4d4a85a";
+ const windowsAmd64Checksum = "6e60ca90a0e3c2e1a596e08556197854d828a7d146f6ec12645ba4cc208e0b19";
 // checksum-end
 
 Future<void> verifyCoreBinary(String filePath) async {
