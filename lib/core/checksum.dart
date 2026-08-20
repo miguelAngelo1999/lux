5e60e503ef6dc9f47aabb4286af518794d17682d
miguelAngelo1999 <miguelAngelo1999@users.noreply.github.com>
2026-08-20 11:34:10 -0300

    chore: resync checksums

--- lib/core/checksum.dart
+++ lib/core/checksum.dart
@@ -5,7 +5,7 @@ import 'package:crypto/crypto.dart';
 // checksum-start
  const darwinAmd64Checksum = "270dcd06f8de7767c0d4cc7a0ab0f61ba47b54f884340442f1bbc052ef35f3d2";
  const darwinArm64Checksum = "1d2e3ebdc05c884df2effc92909300241a9b917f25d7da398f8ebed1fea544ab";
- const windowsAmd64Checksum = "8fc1179661649bfcde241e28976e8bc139d377e8cc599b660f4871ffb15c2146";
+ const windowsAmd64Checksum = "e4644759b9a17b94345ee8f0ccb635ad59adf7acc35587e122a4c8dc59b680c9";
 // checksum-end
 
 Future<void> verifyCoreBinary(String filePath) async {
